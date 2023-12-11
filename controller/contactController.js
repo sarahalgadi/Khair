8cff619091b404468f05bc8fb8e3c72f2192e901
Ralbattah <134811738+Ralbattah@users.noreply.github.com>
2023-12-11 17:53:21 +0300

    Update contactController.js
    fixed names line 6-9 and also parameters.

--- controller/contactController.js
+++ controller/contactController.js
@@ -3,17 +3,17 @@ const Contact = require('../model/contact');
 const { validationResult } = require('express-validator');
  
 const addRequest = (request, response) => {
-    let reason1 = request.body.reason;
-    let name1 = request.body.name;
-    let email1 = request.body.email;
-    let message1 = request.body.message;
+    let reason = request.body.reason;
+    let name = request.body.name;
+    let email = request.body.email;
+    let message = request.body.message;
 
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
         return response.status(422).json({ errors: errors.array() });
     }
 
-    const contact = new Contact({ reason: reason1, name: name1, email: email1, message: message1});
+    const contact = new Contact({ reason: reason, name: name, email: email, message: message});
     contact.save()
         .then((data) => {
             console.log(`Contact saved to database: id -> ${data._id}`);
